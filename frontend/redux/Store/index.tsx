6f386bd456b353646e42dfbb8f8a974eafd0d8e2
Dhimm <139521714+dimasbbs@users.noreply.github.com>
2023-11-29 00:22:42 +0700

    menambahkan reducer all

--- frontend/redux/Store/index.tsx
+++ frontend/redux/Store/index.tsx
@@ -3,11 +3,39 @@ import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "@redux-saga/core";
 import rootSaga from "../Sagas";
 import usersReducers from "../Reducers/Users/usersReducer";
+import HotelAdminReducer from "../Reducers/Hotels/hotelAdminReducer";
+import FaciAdminReducer from "../Reducers/Hotels/faciAdminReducer";
+import RoomNumberReducer from "../Reducers/Hotels/maxRoomIdReducer";
+import AddrHotelReducer from "../Reducers/Hotels/addrHotelReducer";
+import FaphoReducer from "../Reducers/Hotels/faphoAdminReducer";
+import FaciPriceHistoryReducer from "../Reducers/Hotels/fphReducer";
 import departmentReducer from "../Reducers/HumanResources/departmentReducer";
 import employeeReducer from "../Reducers/HumanResources/employeeReducer";
 import shiftReducer from "../Reducers/HumanResources/shiftReducer";
 import jobroleReducer from "../Reducers/HumanResources/jobroleReducer";
 import workorderReducer from "../Reducers/HumanResources/workorderReducer";
+import bankReducer from "../Reducers/payment/bank";
+import fintechReducer from "../Reducers/payment/fintech";
+import transactionReducer from "../Reducers/payment/transaction";
+import userAccountReducer from "../Reducers/payment/userAccount";
+import HotelReducer from "../Reducers/Booking/hotelReducer";
+import FaciAllHotelReducer from "../Reducers/Booking/faciAllHotelReducer";
+import SpecialoffersReducer from "../Reducers/Booking/specialOffersReducer";
+import OrderDetailsReducer from "../Reducers/Booking/orderDetailsReducer";
+import HoreReducer from "../Reducers/Booking/horeReducer";
+import PriceItemsReducer from "../Reducers/Booking/priceItemsReducer";
+import BookingOrderReducer from "../Reducers/Booking/bookingOrderReducer";
+import GetInvoiceReducer from "../Reducers/Booking/getInvoiceReducer";
+import employeePayHistoryReducer from "../Reducers/HumanResources/employeePayHistoryReducer";
+import employeeDepartmentHistoryReducer from "../Reducers/HumanResources/employeeDepartmentHistoryReducer";
+import boexReducer from "../Reducers/Booking/boexReducer";
+import { purchaseOrderReducers } from "../Reducers/Purchasing/purchaseOrderReducers";
+import sphoReducers from "../Reducers/Purchasing/sphoReducers";
+import stocksReducers from "../Reducers/Purchasing/stocksReducers";
+import stodReducers from "../Reducers/Purchasing/stodReducers";
+import vendorReducers from "../Reducers/Purchasing/vendorReducer";
+import vendproReducers from "../Reducers/Purchasing/vendproReducers";
+import workorderdetailReducer from "../Reducers/HumanResources/workorderdetailReducer";
 import masterReducers from "../Reducers/Masters/masterReducer";
 import ContryReducer from "redux/Reducers/Masters/CountryReducer";
 import ProvincesReducer from "redux/Reducers/Masters/ProvincesReducer";
@@ -16,7 +44,15 @@ import policyReducer from "redux/Reducers/Masters/PolicyReducer";
 import PritReducer from "redux/Reducers/Masters/PritReducer";
 import cagroReducer from "redux/Reducers/Masters/CagroReducer";
 import serviceTaskReducer from "redux/Reducers/Masters/serviceTaskReducer";
-import HotelAdminReducer from "../Reducers/Hotels/hotelAdminReducer";
+import bookingHistoryReducer from "../Reducers/Booking/bookingHistory";
+import orderMenusIdAkhirReducers from "../Reducers/Resto/orderMenusIdAkhir";
+import orderDetailReducers from "../Reducers/Resto/orderDetailReducers";
+import orderMenusAkhirReducers from "../Reducers/Resto/orderMenusOne";
+import orderMenusReducers from "../Reducers/Resto/orderMenusReducers";
+import restoCardClientReducers from "../Reducers/Resto/restoCardClientReducer";
+import restoMenusPhotosUrlReducers from "../Reducers/Resto/restoMenuPhotosUrlReducers";
+import restoMenusReducers from "../Reducers/Resto/restoMenusReducer";
+import restoPhotosReducers from "../Reducers/Resto/restoPhotos";
 
 const saga = createSagaMiddleware();
 const reducer = combineReducers({
@@ -29,26 +65,65 @@ const reducer = combineReducers({
   cagroReducer: cagroReducer,
   PritReducer: PritReducer,
   serviceTaskReducer: serviceTaskReducer,
+
   //Users
   usersReducers: usersReducers,
 
   //HR
-  departmentReducer: departmentReducer,
-  employeeReducer: employeeReducer,
-  shiftReducer: shiftReducer,
-  jobroleReducer: jobroleReducer,
-  workorderReducer: workorderReducer,
+  departmentReducer,
+  employeeReducer,
+  shiftReducer,
+  jobroleReducer,
+  workorderReducer,
+  workorderdetailReducer,
+  employeePayHistoryReducer,
+  employeeDepartmentHistoryReducer,
 
   //Hotels
-  HotelAdminReducer: HotelAdminReducer,
+  HotelAdminReducer,
+  FaciAdminReducer,
+  RoomNumberReducer,
+  AddrHotelReducer,
+  FaphoReducer,
+  FaciPriceHistoryReducer,
+
   //Booking
+  HotelReducer,
+  FaciAllHotelReducer,
+  HoreReducer,
+  SpecialoffersReducer,
+  OrderDetailsReducer,
+  PriceItemsReducer,
+  BookingOrderReducer,
+  GetInvoiceReducer,
+  boexReducer,
+  bookingHistoryReducer,
 
   //Resto
+  orderDetailReducers,
+  orderMenusIdAkhirReducers,
+  orderMenusAkhirReducers,
+  orderMenusReducers,
+  restoCardClientReducers,
+  restoMenusPhotosUrlReducers,
+  restoMenusReducers,
+  restoPhotosReducers,
 
   //Payment
+  transactionReducer,
+  bankReducer,
+  userAccountReducer,
+  fintechReducer,
 
   //Purchase
+  vendorReducers: vendorReducers,
+  purchaseOrderReducers: purchaseOrderReducers,
+  vendproReducers: vendproReducers,
+  stocksReducers: stocksReducers,
+  sphoReducers: sphoReducers,
+  stodReducers: stodReducers,
 });
+
 const store = configureStore({
   reducer,
   middleware: (getDefaultMiddleware) =>
